950ca2ec6accbd71c55d7f4f530638b541d63057
Nelesh Bisht <neleshbisht@gmail.com>
2026-02-27 17:39:25 +0530

    Set API base to Render

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://localhost:5000";
+export const API_BASE = "https://hirehub-69wc.onrender.com";
 
 const API = axios.create({
   baseURL: `${API_BASE}/api`,
