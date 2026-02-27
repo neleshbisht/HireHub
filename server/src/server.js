003288eaf36eb7e8e9f9338fb7a99643574a0c4c
Nelesh Bisht <neleshbisht@gmail.com>
2026-02-27 17:47:54 +0530

    Add Vercel production URL to CORS

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,15 @@ const app = express();
 /* ==============================
    Basic Middleware
 ============================== */
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://hire-hub-eight-alpha.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "2mb" }));
 app.use(morgan("dev"));
 
